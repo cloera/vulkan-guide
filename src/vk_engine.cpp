738639cbaaa14c6a9c0b79d4f4e3516cb1113f9b
Christian Loera <loera.christian1@gmail.com>
2026-01-27 13:20:27 -0700

    Initialized Vulkan and slight refactor

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -4,47 +4,58 @@
 #include <SDL.h>
 #include <SDL_vulkan.h>
 
+#include <VkBootstrap.h>
 #include <vk_initializers.h>
-#include <vk_types.h>
 
 #include <chrono>
 #include <thread>
 
-VulkanEngine* loadedEngine = nullptr;
+VulkanEngine* pLoadedEngine = nullptr;
 
-VulkanEngine& VulkanEngine::Get() { return *loadedEngine; }
+VulkanEngine& VulkanEngine::Get() { return *pLoadedEngine; }
 void VulkanEngine::init()
 {
     // only one engine initialization is allowed with the application.
-    assert(loadedEngine == nullptr);
-    loadedEngine = this;
+    assert(pLoadedEngine == nullptr);
+    pLoadedEngine = this;
 
     // We initialize SDL and create a window with it.
     SDL_Init(SDL_INIT_VIDEO);
 
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN);
 
-    _window = SDL_CreateWindow(
+    pWindow = SDL_CreateWindow(
         "Vulkan Engine",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        _windowExtent.width,
-        _windowExtent.height,
+        windowExtent.width,
+        windowExtent.height,
         window_flags);
 
+    // Initialize Vulkan
+    initVulkan();
+    initSwapchain();
+    initCommands();
+    initSyncStructures();
+
     // everything went fine
-    _isInitialized = true;
+    isInitialized = true;
 }
 
 void VulkanEngine::cleanup()
 {
-    if (_isInitialized) {
+    if (isInitialized) {
 
-        SDL_DestroyWindow(_window);
+        SDL_DestroyWindow(pWindow);
+        instance = nullptr;
+        debugMessenger = nullptr;
+        chosenGPU = nullptr;
+        device = nullptr;
+        surface = nullptr;
     }
 
     // clear engine pointer
-    loadedEngine = nullptr;
+    pLoadedEngine = nullptr;
 }
 
 void VulkanEngine::draw()
@@ -67,16 +78,21 @@ void VulkanEngine::run()
 
             if (e.type == SDL_WINDOWEVENT) {
                 if (e.window.event == SDL_WINDOWEVENT_MINIMIZED) {
-                    stop_rendering = true;
+                    stopRendering = true;
                 }
                 if (e.window.event == SDL_WINDOWEVENT_RESTORED) {
-                    stop_rendering = false;
+                    stopRendering = false;
                 }
+            }
+
+            if (e.type == SDL_KEYDOWN)
+            {
+                fmt::print("{}", (char)e.key.keysym.sym);
             }
         }
 
         // do not draw if we are minimized
-        if (stop_rendering) {
+        if (stopRendering) {
             // throttle the speed to avoid the endless spinning
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
             continue;
@@ -84,4 +100,69 @@ void VulkanEngine::run()
 
         draw();
     }
+}
+
+void VulkanEngine::initVulkan()
+{
+    vkb::InstanceBuilder vkInstBuilder;
+
+    // Create Vulkan instance with default debug features
+    vkb::Result<vkb::Instance> builderResult = vkInstBuilder.set_app_name("Sample Vulkan App")
+        .request_validation_layers(requestValidationLayers)
+        .use_default_debug_messenger()
+        .require_api_version(1, 3, 0)
+        .build();
+
+    // Vulkan Bootstrap instance
+    vkb::Instance vkbInstance = builderResult.value();
+
+    // Store Vulkan instance
+    instance = vkbInstance.instance;
+    debugMessenger = vkbInstance.debug_messenger;
+
+    // Create Vulkan specific surface
+    // Surface stores pixels in main memory. Pixels can be accessed and modify with CPU
+    SDL_Vulkan_CreateSurface(pWindow, instance, &surface);
+
+    // Vulkan 1.3 features
+    VkPhysicalDeviceVulkan13Features features { .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES };
+    features.dynamicRendering = true;
+    features.synchronization2 = true;
+
+    // Vulkan 1.2 features
+    VkPhysicalDeviceVulkan12Features features12 { .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES };
+    features12.bufferDeviceAddress = true;
+    features12.descriptorIndexing = true;
+
+    // Use vkbootstrap to select a gpu. 
+    // We want a gpu that can write to the SDL surface and supports vulkan 1.3 with the correct features
+    vkb::PhysicalDeviceSelector selector(vkbInstance);
+    vkb::PhysicalDevice physicalDevice = selector
+        .set_minimum_version(1, 3)
+        .set_required_features_13(features)
+        .set_required_features_12(features12)
+        .set_surface(surface)
+        .select()
+        .value();
+
+    //create the final vulkan device
+    vkb::DeviceBuilder deviceBuilder(physicalDevice);
+
+    vkb::Device vkbDevice = deviceBuilder.build().value();
+
+    // Get the VkDevice handle used in the rest of a vulkan application
+    device = vkbDevice.device;
+    chosenGPU = physicalDevice.physical_device;
+}
+
+void VulkanEngine::initSwapchain()
+{
+}
+
+void VulkanEngine::initCommands()
+{
+}
+
+void VulkanEngine::initSyncStructures()
+{
 }
--- src/vk_engine.h
+++ src/vk_engine.h
@@ -9,26 +9,37 @@
 class VulkanEngine {
 public:
 
-	bool _isInitialized{ false };
-	int _frameNumber {0};
-	bool stop_rendering{ false };
-	VkExtent2D _windowExtent{ 1700 , 900 };
+	VkInstance instance = nullptr;
+	VkDebugUtilsMessengerEXT debugMessenger = nullptr;
+	VkPhysicalDevice chosenGPU = nullptr;
+	VkDevice device = nullptr;
+	VkSurfaceKHR surface = nullptr;
+	VkExtent2D windowExtent { 1700 , 900 };
+	int frameNumber = 0 ;
+	bool isInitialized = false ;
+	bool stopRendering = false ;
+	bool requestValidationLayers = true ;
+	bool padding4;
 
-	struct SDL_Window* _window{ nullptr };
+	struct SDL_Window* pWindow{ nullptr };
 
 	static VulkanEngine& Get();
 
 	//initializes everything in the engine
 	void init();
-
 	//shuts down the engine
 	void cleanup();
-
 	//draw loop
 	void draw();
-
 	//run main loop
 	void run();
+
+private:
+	
+	void initVulkan();
+	void initSwapchain();
+	void initCommands();
+	void initSyncStructures();
 };
 
 #endif // VK_ENGINE_H
